f56f52f9ce8bbcdff70e2e83126008768c6ac9c1
MMagdy <{ID}+{username}@users.noreply.github.com>
2022-05-27 21:00:17 +0300

    wip orders and customers

--- backend/src/Core/Application/Operation/Orders/CreateCashOrderRequest.cs
+++ backend/src/Core/Application/Operation/Orders/CreateCashOrderRequest.cs
@@ -1,37 +1,78 @@
+using FSH.WebApi.Application.Catalog.ServiceCatalogs;
 using FSH.WebApi.Domain.Operation;
+using Mapster;
 
 namespace FSH.WebApi.Application.Operation.Orders;
 
-public class OrderItemRequest {}
+public class OrderItemRequest
+{
+  public Guid ItemId { get; set; }
+}
 
 public class CreateCashOrderRequest
 {
-  public List<OrderItemRequest> Type { get; set; }
+  public List<OrderItemRequest> Items { get; set; }
 }
 
 public class CreateCashOrderRequestValidator : CustomValidator<CreateCashOrderRequest>
 {
-  public CreateCashOrderRequestValidator(IReadRepository<Order> repository, IStringLocalizer<CreateCashOrderRequestValidator> T) =>
-    RuleFor(p => p.Name)
-      .NotEmpty()
-      .MaximumLength(75)
-      .MustAsync(async (name, ct) => await repository.GetBySpecAsync(new OrderByNameSpec(name), ct) is null)
-      .WithMessage((_, name) => T["Order {0} already Exists.", name]);
+  public CreateCashOrderRequestValidator(IReadRepository<Order> repository,
+    IStringLocalizer<CreateCashOrderRequestValidator> T) =>
+    RuleFor(p => p.Items)
+      .NotEmpty();
+}
+
+public class GetDefaultCashCustomerSpec : Specification<Customer>, ISingleResultSpecification
+{
+  public GetDefaultCashCustomerSpec() => Query.Where(a => a.CashDefault);
+}
+
+public class GetServiceCatalogDetailByIdSpec : Specification<ServiceCatalog, ServiceCatalogDto>, ISingleResultSpecification
+{
+  public GetServiceCatalogDetailByIdSpec(Guid serviceCatalogId) =>
+    Query
+      .Include(a => a.Product)
+      .Include(a => a.Service)
+      .Where(a => a.Id == serviceCatalogId);
 }
 
 public class CreateCashOrderRequestHandler : IRequestHandler<CreateCashOrderRequest, OrderDto>
 {
   // Add Domain Events automatically by using IRepositoryWithEvents
   private readonly IRepositoryWithEvents<Order> _repository;
+  private readonly IReadRepository<Customer> _customerRepo;
+  private readonly IReadRepository<ServiceCatalog> _serviceCatalogRepo;
+  private readonly IRepository<OrderItem> _orderItemRepo;
 
-  public CreateCashOrderRequestHandler(IRepositoryWithEvents<Order> repository) => _repository = repository;
+  public CreateCashOrderRequestHandler(IRepositoryWithEvents<Order> repository, IReadRepository<Customer> customerRepo,
+    IRepository<OrderItem> orderItemRepo, IReadRepository<ServiceCatalog> serviceCatalogRepo)
+  {
+    _repository = repository;
+    _customerRepo = customerRepo;
+    _orderItemRepo = orderItemRepo;
+    _serviceCatalogRepo = serviceCatalogRepo;
+  }
 
   public async Task<OrderDto> Handle(CreateCashOrderRequest request, CancellationToken cancellationToken)
   {
-    var brand = new Order(request.Name, request.Description);
+    var defaultCustomer = await _customerRepo.GetBySpecAsync(new GetDefaultCashCustomerSpec(), cancellationToken);
+    if (defaultCustomer is null)
+    {
+      throw new NotFoundException(nameof(defaultCustomer));
+    }
 
-    await _repository.AddAsync(brand, cancellationToken);
+    var order = new Order(defaultCustomer.Id);
+    await _repository.AddAsync(order, cancellationToken);
 
-    return brand;
+    var items = new List<OrderItem>();
+    foreach (OrderItemRequest item in request.Items)
+    {
+      var serviceItem =  (ISingleResultSpecification<ServiceCatalog, ServiceCatalogDto>)(await _serviceCatalogRepo.GetBySpecAsync(new GetServiceCatalogDetailByIdSpec(item.ItemId), cancellationToken));
+      items.Add(new OrderItem(serviceItem, item.ItemId));
+    }
+
+    await _orderItemRepo.AddAsync(items);
+
+    return order.Adapt<OrderDto>();
   }
 }
--- backend/src/Core/Domain/Operation/Customer.cs
+++ backend/src/Core/Domain/Operation/Customer.cs
@@ -4,17 +4,20 @@ public class Customer : BaseEntity, IAggregateRoot
 {
   public string Name { get; private set; }
   public string PhoneNumber { get; private set; }
+  public bool CashDefault { get; private set; }
 
-  public Customer(string name, string phoneNumber)
+  public Customer(string name, string phoneNumber, bool cashDefault)
   {
     Name = name;
     PhoneNumber = phoneNumber;
+    CashDefault = cashDefault;
   }
 
-  public Customer Update(string? name, string? phoneNumber)
+  public Customer Update(string? name, string? phoneNumber, bool? cashDefault)
   {
     if (name is not null && Name?.Equals(name) is not true) Name = name;
     if (phoneNumber is not null && PhoneNumber?.Equals(phoneNumber) is not true) PhoneNumber = phoneNumber;
+    if (cashDefault is not null && CashDefault != cashDefault) CashDefault = cashDefault.Value;
     return this;
   }
 }
