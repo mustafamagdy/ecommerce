db770dc105e1d5092d8650397ace22a2b712d246
MMagdy <{ID}+{username}@users.noreply.github.com>
2022-05-27 12:26:06 +0300

    wip services and service categories

--- backend/src/Infrastructure/Persistence/Initialization/DatabaseInitializer.cs
+++ backend/src/Infrastructure/Persistence/Initialization/DatabaseInitializer.cs
@@ -9,71 +9,71 @@ namespace FSH.WebApi.Infrastructure.Persistence.Initialization;
 
 internal class DatabaseInitializer : IDatabaseInitializer
 {
-    private readonly TenantDbContext _tenantDbContext;
-    private readonly IServiceProvider _serviceProvider;
-    private readonly ILogger<DatabaseInitializer> _logger;
+  private readonly TenantDbContext _tenantDbContext;
+  private readonly IServiceProvider _serviceProvider;
+  private readonly ILogger<DatabaseInitializer> _logger;
 
-    public DatabaseInitializer(TenantDbContext tenantDbContext, IServiceProvider serviceProvider, ILogger<DatabaseInitializer> logger)
+  public DatabaseInitializer(TenantDbContext tenantDbContext, IServiceProvider serviceProvider, ILogger<DatabaseInitializer> logger)
+  {
+    _tenantDbContext = tenantDbContext;
+    _serviceProvider = serviceProvider;
+    _logger = logger;
+  }
+
+  public async Task InitializeDatabasesAsync(CancellationToken cancellationToken)
+  {
+    await InitializeTenantDbAsync(cancellationToken);
+
+    foreach (var tenant in await _tenantDbContext.TenantInfo.ToListAsync(cancellationToken))
     {
-        _tenantDbContext = tenantDbContext;
-        _serviceProvider = serviceProvider;
-        _logger = logger;
+      await InitializeApplicationDbForTenantAsync(tenant, cancellationToken);
     }
 
-    public async Task InitializeDatabasesAsync(CancellationToken cancellationToken)
+    _logger.LogInformation("For documentations and guides, visit https://www.fullstackhero.net");
+    _logger.LogInformation("To Sponsor this project, visit https://opencollective.com/fullstackhero");
+  }
+
+  public async Task InitializeApplicationDbForTenantAsync(FSHTenantInfo tenant, CancellationToken cancellationToken)
+  {
+    // First create a new scope
+    using var scope = _serviceProvider.CreateScope();
+
+    // Then set current tenant so the right connectionstring is used
+    _serviceProvider.GetRequiredService<IMultiTenantContextAccessor>()
+      .MultiTenantContext = new MultiTenantContext<FSHTenantInfo>()
     {
-        await InitializeTenantDbAsync(cancellationToken);
+      TenantInfo = tenant
+    };
 
-        foreach (var tenant in await _tenantDbContext.TenantInfo.ToListAsync(cancellationToken))
-        {
-            await InitializeApplicationDbForTenantAsync(tenant, cancellationToken);
-        }
+    // Then run the initialization in the new scope
+    await scope.ServiceProvider.GetRequiredService<ApplicationDbInitializer>()
+      .InitializeAsync(cancellationToken);
+  }
 
-        _logger.LogInformation("For documentations and guides, visit https://www.fullstackhero.net");
-        _logger.LogInformation("To Sponsor this project, visit https://opencollective.com/fullstackhero");
+  private async Task InitializeTenantDbAsync(CancellationToken cancellationToken)
+  {
+    if ((await _tenantDbContext.Database.GetPendingMigrationsAsync(cancellationToken)).Any())
+    {
+      _logger.LogInformation("Applying Root Migrations");
+      await _tenantDbContext.Database.MigrateAsync(cancellationToken);
     }
 
-    public async Task InitializeApplicationDbForTenantAsync(FSHTenantInfo tenant, CancellationToken cancellationToken)
+    await SeedRootTenantAsync(cancellationToken);
+  }
+
+  private async Task SeedRootTenantAsync(CancellationToken cancellationToken)
+  {
+    if (await _tenantDbContext.TenantInfo.FindAsync(new object?[] { MultitenancyConstants.Root.Id }, cancellationToken: cancellationToken) is null)
     {
-        // First create a new scope
-        using var scope = _serviceProvider.CreateScope();
+      var rootTenant = new FSHTenantInfo(
+        MultitenancyConstants.Root.Id,
+        MultitenancyConstants.Root.Name,
+        string.Empty,
+        MultitenancyConstants.Root.EmailAddress);
 
-        // Then set current tenant so the right connectionstring is used
-        _serviceProvider.GetRequiredService<IMultiTenantContextAccessor>()
-            .MultiTenantContext = new MultiTenantContext<FSHTenantInfo>()
-            {
-                TenantInfo = tenant
-            };
+      _tenantDbContext.TenantInfo.Add(rootTenant);
 
-        // Then run the initialization in the new scope
-        await scope.ServiceProvider.GetRequiredService<ApplicationDbInitializer>()
-            .InitializeAsync(cancellationToken);
+      await _tenantDbContext.SaveChangesAsync(cancellationToken);
     }
-
-    private async Task InitializeTenantDbAsync(CancellationToken cancellationToken)
-    {
-        if ((await _tenantDbContext.Database.GetPendingMigrationsAsync(cancellationToken)).Any())
-        {
-            _logger.LogInformation("Applying Root Migrations");
-            await _tenantDbContext.Database.MigrateAsync(cancellationToken);
-        }
-
-        await SeedRootTenantAsync(cancellationToken);
-    }
-
-    private async Task SeedRootTenantAsync(CancellationToken cancellationToken)
-    {
-        if (await _tenantDbContext.TenantInfo.FindAsync(new object?[] { MultitenancyConstants.Root.Id }, cancellationToken: cancellationToken) is null)
-        {
-            var rootTenant = new FSHTenantInfo(
-                MultitenancyConstants.Root.Id,
-                MultitenancyConstants.Root.Name,
-                string.Empty,
-                MultitenancyConstants.Root.EmailAddress);
-
-            _tenantDbContext.TenantInfo.Add(rootTenant);
-
-            await _tenantDbContext.SaveChangesAsync(cancellationToken);
-        }
-    }
+  }
 }
--- backend/src/Multitenancy/Finbuckle.MultiTenant.AspNetCore/Extensions/FinbuckleMultiTenantBuilderExtensions.cs
+++ backend/src/Multitenancy/Finbuckle.MultiTenant.AspNetCore/Extensions/FinbuckleMultiTenantBuilderExtensions.cs
@@ -89,7 +89,7 @@ namespace Microsoft.Extensions.DependencyInjection
                     {
                         var loggerFactory = context.HttpContext.RequestServices.GetService<ILoggerFactory>();
                         loggerFactory?.CreateLogger<FinbuckleMultiTenantBuilder<TTenantInfo>>()
-                            .LogWarning("No tenant found in authentication properties.");
+                            .LogWarning("No tenant found in authentication properties");
                     }
 
                     // Does the current tenant match the auth property tenant?
