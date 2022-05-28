3ad2303dd146b0c0b96c3b5bd4c1a3790d11ddaf
MMagdy <{ID}+{username}@users.noreply.github.com>
2022-05-28 14:51:25 +0300

    search orders by customer phone number

--- backend/src/Core/Application/Operation/Customers/CustomerDto.cs
+++ backend/src/Core/Application/Operation/Customers/CustomerDto.cs
@@ -2,4 +2,6 @@ namespace FSH.WebApi.Application.Operation.Customers;
 
 public class CustomerDto : IDto
 {
+  public string PhoneNumber { get; set; }
+  public string Name { get; set; }
 }
--- backend/src/Core/Application/Operation/Orders/OrderDto.cs
+++ backend/src/Core/Application/Operation/Orders/OrderDto.cs
@@ -6,6 +6,7 @@ public class OrderDto : IDto
   public string OrderNumber { get; set; }
   public string OrderDate { get; set; }
   public string OrderTime { get; set; }
+  public string PhoneNumber { get; set; }
   public string CustomerName { get; set; }
   public List<OrderItemDto> OrderItems { get; set; }
 }
