b808b4e213c90199d115364ddf2291414f0a9c97
MMagdy <{ID}+{username}@users.noreply.github.com>
2022-05-28 14:51:25 +0300

    search orders by customer phone number

--- backend/src/Core/Application/Operation/Orders/SearchOrdersRequest.cs
+++ backend/src/Core/Application/Operation/Orders/SearchOrdersRequest.cs
@@ -5,6 +5,8 @@ namespace FSH.WebApi.Application.Operation.Orders;
 public class SearchOrdersRequest : PaginationFilter, IRequest<PaginationResponse<OrderDto>>
 {
   public string? OrderNumber { get; set; }
+  public Guid? CustomerId { get; set; }
+  public string? CustomerPhoneNumber { get; set; }
 }
 
 public class OrdersBySearchRequestSpec : EntitiesByPaginationFilterSpec<Order, OrderDto>
@@ -12,7 +14,10 @@ public class OrdersBySearchRequestSpec : EntitiesByPaginationFilterSpec<Order, O
   public OrdersBySearchRequestSpec(SearchOrdersRequest request)
     : base(request) =>
     Query
+      .Include(a => a.Customer)
       .Where(a => request.OrderNumber == null || a.OrderNumber == request.OrderNumber)
+      .Where(a => request.CustomerId == null || a.CustomerId == request.CustomerId)
+      .Where(a => request.CustomerPhoneNumber == null || a.Customer.PhoneNumber == request.CustomerPhoneNumber)
       .OrderBy(c => c.OrderNumber, !request.HasOrderBy());
 }
 
