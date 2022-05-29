6ad8999f640049a6e2b9c6b1f8938bf5fe748595
MMagdy <{ID}+{username}@users.noreply.github.com>
2022-05-29 19:37:30 +0300

    start working on the pdf writer

---
+++ backend/src/Infrastructure/Common/Export/Pdf/PdfWriter.cs
@@ -0,0 +1,11 @@
+﻿using FSH.WebApi.Application.Common.Exporters;
+
+namespace FSH.WebApi.Infrastructure.Common.Export.Pdf;
+
+public class PdfWriter : IPdfWriter
+{
+  public Stream WriteToStream(dynamic data)
+  {
+    return null;
+  }
+}
