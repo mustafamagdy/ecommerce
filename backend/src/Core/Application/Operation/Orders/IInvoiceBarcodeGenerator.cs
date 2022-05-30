9988052d79ea901dce8e7e3e5fe8c3654fe08b8f
MMagdy <{ID}+{username}@users.noreply.github.com>
2022-05-30 19:35:42 +0300

    invoice barcode is working

--- backend/src/Core/Application/Operation/Orders/IInvoiceBarcodeGenerator.cs
+++ backend/src/Core/Application/Operation/Orders/IInvoiceBarcodeGenerator.cs
@@ -2,9 +2,9 @@ using SkiaSharp.QrCode.Image;
 
 namespace FSH.WebApi.Application.Operation.Orders;
 
-public interface IInvoiceBarcodeGenerator
+public interface IInvoiceBarcodeGenerator : ITransientService
 {
-  Stream GenerateQrCode(IInvoiceBarcodeInfo info, int width, int height);
+  byte[] GenerateQrCode(IInvoiceBarcodeInfo info, int width, int height);
   void SaveQrCode(QrCode qrcodeImage, string fullFilePath);
   string ToBase64(IInvoiceBarcodeInfo info);
 }
