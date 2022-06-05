f6d7101927a576d0c4be050512e22295b7246116
MMagdy <{ID}+{username}@users.noreply.github.com>
2022-06-05 20:55:34 +0300

    wip setup demo for permission

---
+++ backend/src/Host/Controllers/DemoPermissions/Demo1.cs
@@ -0,0 +1,69 @@
+﻿using FSH.WebApi.Application.Catalog.Brands;
+using FSH.WebApi.Application.Dashboard;
+using FSH.WebApi.Application.Identity.Roles;
+using FSH.WebApi.Application.Identity.Users;
+
+namespace FSH.WebApi.Host.Controllers.DemoPermissions;
+
+public class Demo1 : VersionedApiController
+{
+  private readonly IUserService _userService;
+  private readonly IRoleService _roleService;
+
+  public Demo1(IUserService userService, IRoleService roleService)
+  {
+    _userService = userService;
+    _roleService = roleService;
+  }
+
+  [HttpGet]
+  [MustHavePermission(FSHAction.View, FSHResource.Brands)]
+  [OpenApiOperation("Setup the permissions demo 1", "")]
+  public async Task Setup()
+  {
+    var user = await _userService.CreateAsync(
+      new CreateUserRequest
+      {
+        UserName = "demo1",
+        Email = "demo1@root.com",
+        Password = "1234@1234"
+      }, "test");
+
+    var role = await _roleService.CreateOrUpdateAsync(new CreateOrUpdateRoleRequest
+    {
+      Name = "user"
+    });
+
+    _userService.AssignRolesAsync(user, new UserRolesRequest
+    {
+      UserRoles = new List<UserRoleDto>()
+      {
+        new() { RoleId = role }
+      }
+    }, CancellationToken.None);
+  }
+
+  [HttpGet("{id:guid}")]
+  [MustHavePermission(FSHAction.View, FSHResource.Brands)]
+  [OpenApiOperation("Get brand details.", "")]
+  public Task<BrandDto> GetAsync(Guid id)
+  {
+    return Mediator.Send(new GetBrandRequest(id));
+  }
+
+  [HttpPost]
+  [MustHavePermission(FSHAction.Create, FSHResource.Brands)]
+  [OpenApiOperation("Create a new brand.", "")]
+  public Task<Guid> CreateAsync(CreateBrandRequest request)
+  {
+    return Mediator.Send(request);
+  }
+
+  [HttpDelete("{id:guid}")]
+  [MustHavePermission(FSHAction.Delete, FSHResource.Brands)]
+  [OpenApiOperation("Delete a brand.", "")]
+  public Task<Guid> DeleteAsync(Guid id)
+  {
+    return Mediator.Send(new DeleteBrandRequest(id));
+  }
+}
