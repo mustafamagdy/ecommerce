2d4940be69418849466d8163df6216b62f8fe1e7
MMagdy <{ID}+{username}@users.noreply.github.com>
2022-07-12 06:38:38 +0200

    basic skelton for testing

--- backend/tests/Application.IntegrationTests/UnitTest1.cs
+++ backend/tests/Application.IntegrationTests/UnitTest1.cs
@@ -2,41 +2,97 @@ using System.Net.Http.Json;
 using FSH.WebApi.Application.Catalog.Products;
 using FSH.WebApi.Application.Common.Models;
 using FSH.WebApi.Application.Identity.Tokens;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json;
 using Xunit;
+using Xunit.Abstractions;
 using JsonConverter = System.Text.Json.Serialization.JsonConverter;
 
 namespace Application.IntegrationTests;
 
 public class TestWebApplicationFactory : WebApplicationFactory<Program>
 {
-  protected override IHostBuilder? CreateHostBuilder()
+  protected override void ConfigureWebHost(IWebHostBuilder builder)
   {
-    return base.CreateHostBuilder();
+    builder.ConfigureServices(services =>
+    {
+    });
+    base.ConfigureWebHost(builder);
   }
 }
-public class UnitTest1
+
+public class HostFixture : IDisposable
 {
-  public UnitTest1()
+  private readonly WebApplicationFactory<Program> _factory;
+  internal readonly HttpClient Client;
+
+  public HostFixture()
+  {
+    _factory = new TestWebApplicationFactory();
+    Client = _factory.CreateClient();
+  }
+
+  public void Dispose()
+  {
+    _factory.Dispose();
+  }
+}
+
+public abstract class TestFixture : IDisposable, IClassFixture<HostFixture>
+{
+  private readonly HostFixture _host;
+  protected readonly ITestOutputHelper _output;
+
+  public TestFixture(HostFixture host, ITestOutputHelper output)
+  {
+    _host = host;
+    _output = output;
+  }
+
+  public Task<HttpResponseMessage> PostAsJsonAsync<TValue>(string? requestUri, TValue value, Dictionary<string, string> headers, CancellationToken cancellationToken = default)
+  {
+    _host.Client.DefaultRequestHeaders.Clear();
+    foreach ((string? key, string? val) in headers)
+    {
+      _host.Client.DefaultRequestHeaders.Add(key, val);
+    }
+
+    return _host.Client.PostAsJsonAsync(requestUri, value, cancellationToken);
+  }
+
+  public void Dispose()
+  {
+    _output.WriteLine("Disposing ...");
+    _host.Dispose();
+  }
+}
+
+public class ExampleTests1 : TestFixture
+{
+  public ExampleTests1(HostFixture host, ITestOutputHelper output)
+    : base(host, output)
   {
   }
 
   [Fact]
   public async Task Test1()
   {
-    var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
-    // var application = new TestWebApplicationFactory();
+    _output.WriteLine("Start testing ..");
+    var response = await PostAsJsonAsync("/api/tokens",
+      new TokenRequest("admin@root.com", "123Pa$$word!"),
+      new Dictionary<string, string> { { "tenant", "root" } });
 
-    var client = application.CreateClient();
-
-    client.DefaultRequestHeaders.Add("tenant", "root");
-    var response = await client.PostAsJsonAsync("/api/tokens", new TokenRequest("admin@root.com", "123Pa$$word!"), CancellationToken.None);
     var tokenResult = await response.Content.ReadFromJsonAsync<TokenResponse>();
 
-    client.DefaultRequestHeaders.Add("Authorization", $"Bearer {tokenResult.Token}");
-    response = await client.PostAsJsonAsync("/api/v1/products/search", new SearchProductsRequest());
+    _output.WriteLine("Token is " + tokenResult.Token);
+
+    response = await PostAsJsonAsync("/api/v1/products/search",
+      new SearchProductsRequest(),
+      new Dictionary<string, string> { { "Authorization", $"Bearer {tokenResult.Token}" } });
+
     if (response.IsSuccessStatusCode)
     {
       var productResult = await response.Content.ReadFromJsonAsync<PaginationResponse<ProductDto>>();
