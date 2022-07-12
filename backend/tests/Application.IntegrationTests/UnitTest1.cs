7854562d57267a7a949dea3ea1c1ca50f7b2c016
MMagdy <{ID}+{username}@users.noreply.github.com>
2022-07-12 05:59:48 +0200

    refactor paginated result to be able to deserialize from json

--- backend/tests/Application.IntegrationTests/UnitTest1.cs
+++ backend/tests/Application.IntegrationTests/UnitTest1.cs
@@ -3,12 +3,20 @@ using FSH.WebApi.Application.Catalog.Products;
 using FSH.WebApi.Application.Common.Models;
 using FSH.WebApi.Application.Identity.Tokens;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json;
 using Xunit;
 using JsonConverter = System.Text.Json.Serialization.JsonConverter;
 
 namespace Application.IntegrationTests;
 
+public class TestWebApplicationFactory : WebApplicationFactory<Program>
+{
+  protected override IHostBuilder? CreateHostBuilder()
+  {
+    return base.CreateHostBuilder();
+  }
+}
 public class UnitTest1
 {
   public UnitTest1()
@@ -19,6 +27,7 @@ public class UnitTest1
   public async Task Test1()
   {
     var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
+    // var application = new TestWebApplicationFactory();
 
     var client = application.CreateClient();
 
