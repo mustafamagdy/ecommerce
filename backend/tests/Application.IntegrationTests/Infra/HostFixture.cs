fb6b4c46db76baafc301ca82bffbdf0597cf6cb1
MMagdy <{ID}+{username}@users.noreply.github.com>
2022-07-14 08:49:47 +0200

    wip for workaround webapplicationfactory override configuration issue

--- backend/tests/Application.IntegrationTests/Infra/HostFixture.cs
+++ backend/tests/Application.IntegrationTests/Infra/HostFixture.cs
@@ -1,4 +1,6 @@
+using System.Reflection;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
 using Xunit.Abstractions;
 
 namespace Application.IntegrationTests;
@@ -10,7 +12,23 @@ public class HostFixture : IDisposable
 
   public HostFixture()
   {
-    _factory = new TestWebApplicationFactory();
+    var dbName = "multi-tenant-09";
+    _factory = new TestWebApplicationFactory()
+        // .WithWebHostBuilder(builder =>
+        // {
+        //   builder.ConfigureAppConfiguration((context, configBuilder) =>
+        //   {
+        //     configBuilder.AddInMemoryCollection(
+        //       new Dictionary<string, string>
+        //       {
+        //         ["DatabaseSettings:ConnectionString"] = $"Data Source=127.0.0.1;Initial Catalog={dbName};User Id=root;Password=DeV12345",
+        //         ["HangfireSettings:Storage:ConnectionString"] = $"Data Source=127.0.0.1;Initial Catalog={dbName};User Id=root;Password=DeV12345;Allow User Variables=true"
+        //       });
+        //     // var path = AppDomain.CurrentDomain.BaseDirectory;
+        //     // configBuilder.AddJsonFile($"{path}/tests-appsettings.json", optional: false, reloadOnChange: true);
+        //   });
+        // })
+      ;
   }
 
   public HttpClient CreateClient() => _factory.CreateClient();
