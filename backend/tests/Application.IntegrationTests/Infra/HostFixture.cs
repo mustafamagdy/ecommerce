39b28e14e4454ef0686fb7622b8ee1dd6eb5400b
MMagdy <{ID}+{username}@users.noreply.github.com>
2022-07-16 11:24:47 +0200

    update server name in tests

--- backend/tests/Application.IntegrationTests/Infra/HostFixture.cs
+++ backend/tests/Application.IntegrationTests/Infra/HostFixture.cs
@@ -16,7 +16,7 @@ public class HostFixture : IDisposable
     var db_name = $"main_{Guid.NewGuid()}";
     DATABASES.Add(db_name);
 
-    _cnStringTemplate = "Data Source=mysql-db;Initial Catalog={0};User Id=root;Password=DeV12345";
+    _cnStringTemplate = "Data Source=127.0.0.1;Initial Catalog={0};User Id=root;Password=DeV12345";
     _memoryConfigs = Program.OverrideConfig(new Dictionary<string, string>
     {
       ["DatabaseSettings:ConnectionString"] = string.Format(_cnStringTemplate, db_name),
