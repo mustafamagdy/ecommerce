e0f4e4ca1b3bcf40a0f2cb5f09500fd1e3adf3c0
MMagdy <{ID}+{username}@users.noreply.github.com>
2022-07-16 12:27:18 +0200

    try different things to make test works

--- backend/tests/Application.IntegrationTests/Infra/HostFixture.cs
+++ backend/tests/Application.IntegrationTests/Infra/HostFixture.cs
@@ -13,10 +13,10 @@ public class HostFixture : IDisposable
 
   public HostFixture()
   {
-    var db_name = $"main_{Guid.NewGuid()}";
+    var db_name = "root-db";// $"main_{Guid.NewGuid()}";
     DATABASES.Add(db_name);
 
-    _cnStringTemplate = "Data Source=127.0.0.1;Initial Catalog={0};User Id=mysql-test;Password=DeV12345;SSL Mode=None;AllowPublicKeyRetrieval=true";
+    _cnStringTemplate = "Data Source=127.0.0.1;Initial Catalog={0};User Id=root;Password=DeV12345;SSL Mode=None;AllowPublicKeyRetrieval=true";
     _memoryConfigs = Program.OverrideConfig(new Dictionary<string, string>
     {
       ["DatabaseSettings:ConnectionString"] = string.Format(_cnStringTemplate, db_name),
