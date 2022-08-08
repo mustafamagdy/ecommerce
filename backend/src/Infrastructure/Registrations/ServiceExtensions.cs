a3b0577d6ccd35502697fbaa83bfc0a22b3e2924
MMagdy <{ID}+{username}@users.noreply.github.com>
2022-08-08 23:30:32 +0300

    refactor program service and mvc builder registeration

---
+++ backend/src/Infrastructure/Registrations/ServiceExtensions.cs
@@ -0,0 +1,39 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using FluentValidation.AspNetCore;
+using FSH.WebApi.Infrastructure.Finance;
+using FSH.WebApi.Infrastructure.Multitenancy;
+using Microsoft.Extensions.DependencyInjection;
+using ZymLabs.NSwag.FluentValidation.AspNetCore;
+
+namespace FSH.WebApi.Infrastructure.Registrations;
+
+public static class ServiceExtensions
+{
+  public static IMvcBuilder AddApplicationControllers(this IServiceCollection services)
+  {
+    return services.AddControllers(opt =>
+    {
+      opt.Filters.Add<HasValidSubscriptionTypeFilter>();
+      opt.Filters.Add<RequireOpenCashRegisterFilter>();
+    });
+  }
+
+  public static IMvcBuilder AddApplicationFluentValidation(this IMvcBuilder builder)
+  {
+    return builder.AddFluentValidation(conf =>
+    {
+      conf.ValidatorFactoryType = typeof(HttpContextServiceProviderValidatorFactory);
+    });
+  }
+
+  public static IMvcBuilder AddApplicationJsonOptions(this IMvcBuilder builder)
+  {
+    return builder.AddJsonOptions(opt =>
+    {
+      opt.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+      opt.JsonSerializerOptions.Converters.Clear();
+      opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase, false));
+    });
+  }
+}
