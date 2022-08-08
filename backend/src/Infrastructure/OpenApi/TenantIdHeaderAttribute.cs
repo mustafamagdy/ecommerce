52d3a089666767e725411824edb05baa7eafad8b
MMagdy <{ID}+{username}@users.noreply.github.com>
2022-08-08 21:23:25 +0300

    refactor require opened cash register header

--- backend/src/Infrastructure/OpenApi/TenantIdHeaderAttribute.cs
+++ backend/src/Infrastructure/OpenApi/TenantIdHeaderAttribute.cs
@@ -5,11 +5,7 @@ namespace FSH.WebApi.Infrastructure.OpenApi;
 public class TenantIdHeaderAttribute : SwaggerHeaderAttribute
 {
   public TenantIdHeaderAttribute()
-    : base(
-      MultitenancyConstants.TenantIdName,
-      "Input your tenant Id to access this API",
-      string.Empty,
-      true)
+    : base(MultitenancyConstants.TenantIdName, "Input your tenant Id to access this API", string.Empty, true)
   {
   }
 }
