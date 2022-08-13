7f21e23c9fbfe68b41d61dd930d177d47ed9315b
MMagdy <{ID}+{username}@users.noreply.github.com>
2022-08-13 18:59:18 +0300

    poc table working

---
+++ backend/src/Migrators/Migrators.MySQL/Migrations/Application/20220813111533_App_InitialMigration1.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Migrators.MySQL.Migrations.Application
+{
+    public partial class App_InitialMigration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ColumnDefs",
+                schema: "Shared",
+                table: "DocumentSection",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "HeaderStyle",
+                schema: "Shared",
+                table: "DocumentSection",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "HeaderTitle",
+                schema: "Shared",
+                table: "DocumentSection",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ColumnDefs",
+                schema: "Shared",
+                table: "DocumentSection");
+
+            migrationBuilder.DropColumn(
+                name: "HeaderStyle",
+                schema: "Shared",
+                table: "DocumentSection");
+
+            migrationBuilder.DropColumn(
+                name: "HeaderTitle",
+                schema: "Shared",
+                table: "DocumentSection");
+        }
+    }
+}
