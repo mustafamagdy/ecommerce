d33e0a3faefeb51f8e635c01d09c530c27eaa6a7
Mohamed Jamal <mohamedjanemr@hotmail.com>
2023-02-14 22:01:54 +0300

    payment methods api

--- backend/src/Host/Controllers/Operation/OrdersController.cs
+++ backend/src/Host/Controllers/Operation/OrdersController.cs
@@ -1,5 +1,6 @@
 ﻿using FSH.WebApi.Application.Common.Interfaces;
 using FSH.WebApi.Application.Operation.Orders;
+using FSH.WebApi.Application.Operation.Payments;
 using FSH.WebApi.Infrastructure.Finance;
 using FSH.WebApi.Shared.Multitenancy;
 
@@ -93,6 +94,14 @@ public sealed class OrdersController : VersionedApiController
     var orderPdf = await Mediator.Send(request, cancellationToken);
     var reportName = $"orders_summary_{time.Now.ToShortDateString()}";
     return File(orderPdf, "application/octet-stream", $"{reportName}.pdf");
+  }
+
+  [HttpGet("payment-methods")]
+  [MustHavePermission(FSHAction.View, FSHResource.Orders)]
+  [OpenApiOperation("Generate orders summary report for requested filters.", "")]
+  public async Task<List<PaymentMethodDto>> GetPaymentMethods( CancellationToken cancellationToken)
+  {
+   return await Mediator.Send(new GetPaymentMethodsRequest(), cancellationToken);
   }
 
   // [HttpPut("{id:guid}")]
